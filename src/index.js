abfb3ed583a4069803cdbe03da6d29adb1a3ca3a
FlorinMazareanu <florin.mazareanu@gmail.com>
2023-03-07 18:53:17 +0200

    3.0.0

--- src/index.js
+++ src/index.js
@@ -40,6 +40,25 @@ let selectedProject = createProjectObject(1, "selected-project");
 //inserting the default "selected-project" data into localStorage
 addProjectInLocalStorage(selectedProject.id, selectedProject);
 
+//hardcoding some projects and tasks to load the page up a bit
+//feel free to add/remove tasks and projects
+let projectOne = createProjectObject(2, "House chores");
+let projectTwo = createProjectObject(3, "Work assignment");
+addProjectInLocalStorage(projectOne.id, projectOne);
+addProjectInLocalStorage(projectTwo.id, projectTwo);
+//let taskOne = createTaskObject(id, title, description, dueDate, inProject, isDone);
+let taskOne = createTaskObject(4, "Walk the dog", "He deserves a treat also", "2023-03-07", "House chores", "no");
+let taskTwo = createTaskObject(5, "Clean the room", "Almost done. I'll mark this complete", "2023-03-07", "House chores", "yes");
+let taskThree = createTaskObject(6, "Wash the car", "There's a great car wash place nearby", "2023-03-07", "House chores", "no");
+let taskFour = createTaskObject(7, "Finish that presentation", "Well, time to work on that", "2023-03-07", "Work assignment", "no");
+let taskFive = createTaskObject(8, "Fix that bug", "Fixed it. I caused it, I fixed it.", "2023-03-07", "Work assignment", "yes");
+addTaskInLocalStorage(4, taskOne);
+addTaskInLocalStorage(5, taskTwo);
+addTaskInLocalStorage(6, taskThree);
+addTaskInLocalStorage(7, taskFour) ;
+addTaskInLocalStorage(8, taskFive);
+
+
 //loading all tasks from all projects:
 loadTasks(0);
 
